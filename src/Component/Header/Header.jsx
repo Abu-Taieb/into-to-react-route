6defdde475e73c92927173b434c8a670c0fc61f2
Abu-Taieb <taieb.contact.bd@gmail.com>
2023-04-05 15:22:43 +0600

    React Rount Project

---
+++ src/Component/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <nav>
+            <Link to="/" className="">Home</Link>
+            <Link to="friends">Friends</Link>
+            <Link to="about" className="">About</Link>
+            <Link to="contact" className="">Contact</Link>
+        </nav>
+    );
+};
+
+export default Header;
